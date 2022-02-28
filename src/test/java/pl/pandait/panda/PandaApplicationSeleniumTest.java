318a627a28320702c01842761f5482a649d7885d
Mateusz Wocka <mateusz.wocka@gmail.com>
2022-02-28 20:05:56 +0100

    Update PandaApplicationSeleniumTest.java

--- src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
+++ src/test/java/pl/pandait/panda/PandaApplicationSeleniumTest.java
@@ -37,7 +37,7 @@ public class PandaApplicationSeleniumTest {
 
         // Pamiętaj, że aplikacja Spring musi działać!
         // driver.get(String.format("http://ubuntucompose:%s/", port));
-        driver.get(String.format("http://192.168.44.44:%d/", 8080));
+        driver.get(String.format("http://192.168.44.44:%s/", "8080"));
         
         // Alternatywnie, gdyby DNS dockera nie komunikował się poprawnie
         // driver.get(String.format("http://192.168.44.44:%s", port));
